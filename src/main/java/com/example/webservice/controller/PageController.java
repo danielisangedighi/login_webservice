db0e110489e2e4f2b1468e51021d87f5062d2c86
Daniel Isangedighi <danielisangedighi@outlook.com>
2024-06-18 21:22:41 +0100

    user page added

--- src/main/java/com/example/webservice/controller/PageController.java
+++ src/main/java/com/example/webservice/controller/PageController.java
@@ -1,10 +1,8 @@
 package com.example.webservice.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.example.webservice.model.User;
 
 
 import ch.qos.logback.core.model.Model;
